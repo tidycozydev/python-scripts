89f01b0131d5511c6072fea8c1d2f459cd875ce1
tidycozydev <tidycozydev@proton.me>
2023-12-17 15:31:38 -0500

    new monitoring script + README.md update

---
+++ monitoring/monitoring-nginx-access-logs.py
@@ -0,0 +1,29 @@
+import argparse, pandas
+
+# Arguments
+argParser =  argparse.ArgumentParser()
+argParser.add_argument("-i", "--ips", help="IPs we don't need to monitor, comma-separated values")
+args = argParser.parse_args()
+
+access_log = open('/var/log/nginx/access.log', 'r')
+
+# Find this line of code here: https://riptutorial.com/pandas/example/15180/read-nginx-access-log--multiple-quotechars-
+# Find the regex here: https://linuxtech.in/efficiently-parsing-nginx-log-files-using-python/
+df = pandas.read_csv(
+        access_log,
+        sep=r'(\S+) - - \[(.*?)\] "(.*?)" (\d+) (\d+) "(.*?)" "(.*?)"',
+        engine='python',
+        usecols=[1, 2, 3, 4, 5, 6, 7],
+        names=['ip', 'time', 'request', 'status', 'size', 'referer', 'user_agent'],
+        na_values='-',
+        header=None)
+
+# Filter IP addresses we don't care about and query for non 200 response status
+ipsToIgnore = args.ips.split(',')
+sub_df = df[~df['ip'].isin(ipsToIgnore)].query('status != 200')
+
+# Sum of non OK status by IP address, order by number of queries
+new_df = sub_df.groupby(['ip', 'status'])['status'].count().sort_values(ascending=False).reset_index(name='count')
+
+# Print in HTML format
+print(new_df.to_html())
